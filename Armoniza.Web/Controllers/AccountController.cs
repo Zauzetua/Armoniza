d84d416c83b90b6b6159218b395511b61d945625
Zauzetua <yisuschuy12345@hotmail.com>
2025-03-12 13:23:42 -0700

    markitos

--- Armoniza.Web/Controllers/AccountController.cs
+++ Armoniza.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ namespace Armoniza.Web.Controllers
 {
     public class AccountController : Controller
     {
+        // Se inyecta el contexto de la base de datos
         private readonly ApplicationDbContext _context;
 
         public AccountController(ApplicationDbContext context)
@@ -26,27 +27,34 @@ namespace Armoniza.Web.Controllers
         [AllowAnonymous]  //Permite acceder al método sin autenticación
         public async Task<IActionResult> Login(string username, string password)
         {
-           
+            // Se busca el usuario en la base de datos
             var admin = _context.Admin.SingleOrDefault(a => a.username == username);
 
+            // Si el usuario no existe o la contraseña no coincide, se muestra un mensaje de error
             if (admin == null || !BCrypt.Net.BCrypt.Verify(password, admin.password))
             {
                 TempData["error"] = "Usuario o contraseña incorrectos."; // Se puede usar TempData para enviar mensajes entre vistas
                 return View();
             }
 
+            // Se crea una lista de claims con el nombre del usuario
+            //Los claims son una forma de representar la identidad de un usuario
             var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, admin.username)
             };
 
+            // Se crea un objeto ClaimsIdentity con los claims
             var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            // Se crea un objeto ClaimsPrincipal con el objeto ClaimsIdentity
             var principal = new ClaimsPrincipal(identity);
 
+            // Se inicia sesión con el objeto ClaimsPrincipal
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
 
+
             TempData["success"] = "¡Inicio sesion exitosamente!"; // Se puede usar TempData para enviar mensajes entre vistas
-            
+            // Se redirige a la página de inicio
             return RedirectToAction("Index", "Home");
 
 
@@ -54,6 +62,7 @@ namespace Armoniza.Web.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            // Se cierra la sesión
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login");
         }

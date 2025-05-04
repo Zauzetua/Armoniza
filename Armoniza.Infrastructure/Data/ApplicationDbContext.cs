9dd22c3ff9ba2a85bb51262a6ef9b1cc6492fac1
kikeytz <enriquecreed04@gmail.com>
2025-05-03 17:13:10 -0700

    Agregue un login mas moderno

--- Armoniza.Infrastructure/Data/ApplicationDbContext.cs
+++ Armoniza.Infrastructure/Data/ApplicationDbContext.cs
@@ -40,7 +40,7 @@ public partial class ApplicationDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Armoniza_Test;Username=postgres;Password=1001");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Armoniza_Test;Username=postgres;Password=123");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
